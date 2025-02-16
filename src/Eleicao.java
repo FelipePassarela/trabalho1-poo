a3c89f0ef048080b2c32f8190f58f7969eec7e65
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2025-02-16 07:52:25 -0300

    Remove unused imports

--- src/Eleicao.java
+++ src/Eleicao.java
@@ -1,13 +1,11 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 public class Eleicao {
     private static final Eleicao INSTANCE = new Eleicao();
--- src/Partido.java
+++ src/Partido.java
@@ -1,7 +1,5 @@
 import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
